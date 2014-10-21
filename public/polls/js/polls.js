92ce3d97a460a0ab2414843caedfcc2bdeafe0b7
opensoars <sam_1700@hotmail.com>
2014-10-21 17:05:17 +0200

    Added legit html structure

---
+++ public/polls/js/polls.js
@@ -0,0 +1,42 @@
+var POLLS_URL = '/rest/polls';
+
+var pollsList = document.getElementById('pollsList');
+
+function drawPoll(poll){
+  pollsList.innerHTML += "<li><a href='/poll.html?id=" + poll.id + "'>"
+    + JSON.stringify(poll) + "</a></li>";
+}
+
+function handlePolls(polls){
+  pollsList.innerHTML = '';
+
+  if(polls.length === 0){
+    pollsList.innerHTML = 'There are no polls!';
+  }
+  else polls.forEach(drawPoll);
+}
+
+function getPolls(){
+
+  var req = new XMLHttpRequest();
+
+  req.onabort = function (){
+    pollsList.innerHTML = 'Could not GET polls';
+  };
+
+  req.onreadystatechange = function (){
+    if(this.readyState === 4){
+      if(this.status === 200){
+        handlePolls(JSON.parse(this.response));
+      }
+      else pollsList.innerHTML = 'Could not GET polls';
+    }
+
+  };
+
+  req.open('GET', POLLS_URL, true);
+  req.send('');
+}
+
+getPolls();
+setInterval(function (){ getPolls(); }, 2500);
