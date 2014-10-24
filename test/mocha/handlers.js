71c7ab6848e8fdbe0be3164e5f1c309dd3029a0e
opensoars <sam_1700@hotmail.com>
2014-10-24 15:46:42 +0200

    Completed get_polls_by_id 100% coverage test

--- test/mocha/handlers.js
+++ test/mocha/handlers.js
@@ -63,5 +63,54 @@ describe('handlers', function (){
     });
   });
 
+  describe('#get_polls_by_id', function (){
+    it('should return the poll we just created with the id of 0', function (done){
+      var o = { hostname: 'localhost', path: '/rest/polls', port: 80, method: 'POST' };
+
+      var req = http.request(o, function (res){
+
+        res.on('data', function (){
+
+          http.get('http://localhost/rest/polls/0', function (res){
+            var d = ''; res.on('data', function (c){ d += c; });
+            res.on('end', function (){
+              var poll = JSON.parse(d);
+              assert.equal(poll.id, 0);
+              assert.equal(poll.title, 'Hello World!');
+              assert.equal(poll.options[0], 'a');
+              assert.equal(poll.options[1], 'b');
+              done();
+            });
+          });
+
+        });
+      });
+
+      req.on('error', function (){ throw 'POST request failed!'; });
+
+      req.write(JSON.stringify({ title: 'Hello World!', options: ['a', 'b'] }));
+      req.end();
+    });
+
+    it('should return a 404 when the poll was not found', function (done){
+      http.get('http://localhost/rest/polls/500', function (res){
+        var d = ''; res.on('data', function (c){ d += c; });
+        res.on('end', function (){
+
+          d = JSON.parse(d);
+
+          assert.equal(d.status, 'failed')
+          assert.equal(d.desc, 'Could not find poll: 500');
+          assert.equal(res.statusCode, 404);
+
+          done();
+
+        });
+      })
+    })
+
+  });
+
+
 });
 
