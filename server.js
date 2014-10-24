fd81443f277bf58e335a2e9d7c0c6f8df223b834
opensoars <sam_1700@hotmail.com>
2014-10-24 18:01:52 +0200

    Re-did future and current package.json

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ app.post('/rest/vote/:id', handlers.post_vote_by_id);
 app.listen(PORT);
 log('Listening at port: ' + PORT);
 
+app.request('asd');
+
 module.exports = {
   status: 'succes',
   port: PORT
