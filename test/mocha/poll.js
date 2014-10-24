8ac9537031c1153e25499305e9599cb03e358eb6
opensoars <sam_1700@hotmail.com>
2014-10-24 14:56:41 +0200

    Fundamental change in o passing / parsing

--- test/mocha/poll.js
+++ test/mocha/poll.js
@@ -12,44 +12,53 @@ describe('Poll', function (){
 
     it('should throw "No o.options found" when we do not give o.data', function (){
       try{ var poll = new Poll({}); }
-      catch(e){ assert.equal(e, 'No o.data found'); }
+      catch(e){ assert.equal(e, 'No o.options found'); }
     });
 
 
-    it('o.data.ip should default to false', function (){
-      var poll = new Poll({ data: {options: ['a', 'b']} });
+    it('o.ip should default to false', function (){
+      var poll = new Poll({ options: ['a', 'b'] });
       assert.equal(poll.ip, false);
     });
 
     it('o.data.multi should default to false', function (){
-      var poll = new Poll({ data: {options: ['a', 'b']} });
+      var poll = new Poll({ options: ['a', 'b'] });
       assert.equal(poll.multi, false);
     });
 
 
     it('should set o.data.ip to true when it passed', function (){
-      var poll = new Poll({ data: {options: ['a', 'b'], ip: true} });
+      var poll = new Poll({ options: ['a', 'b'], ip: true });
       assert.equal(poll.ip, true);
     });
 
     it('should set o.data.multi to true when it passed', function (){
-      var poll = new Poll({ data: {options: ['a', 'b'], multi: true} });
+      var poll = new Poll({ options: ['a', 'b'], multi: true });
       assert.equal(poll.multi, true);
     });
 
 
     it('should parse a JSON string to native JS', function (){
-      var poll = new Poll({ data: JSON.stringify({options: ['a', 'b']}) });
+      var poll = new Poll( JSON.stringify({options: ['a', 'b']}) );
     });
 
   });
 
   describe('#active pinger', function (){
-    it('should ping checkActive', function (done){
-      var poll = new Poll({ timeout: 100, data: JSON.stringify({options: ['a', 'b']}) });
 
-      setTimeout(function (){ done(); }, 1100);
+    it('should have set timeout to the default 60000 when no timeout.config argument is given', function (){
+      var poll = new Poll({ options: ['a', 'b'] });
+      assert.equal(poll.timeout, 60000);
     });
+
+    it('should have set timeout to 100 when we pass the config.timeout argument', function (done){
+      var poll = new Poll({
+        config: {timeout: 100},
+        data: JSON.stringify({options: ['a', 'b']})
+      });
+      assert.equal(poll.timeout, 100);
+    });
+
   });
 
 });
